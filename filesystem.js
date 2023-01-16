c1cf17c22a4888a80f0c9c59d07deb436aa64284
Mirko Richter <mirko.richter@mgm-sp.com>
2023-01-16 07:41:13 +0100

    initial functionalities

---
+++ filesystem.js
@@ -0,0 +1,65 @@
+/*\
+title: $:/plugins/yaml-frontmattered-markdown/filesystem.js
+type: application/javascript
+module-type: utils-node
+
+Filesystem-Delegate for supporting YAML-frontmattered markdown
+
+\*/
+(function(){
+
+// store the previous save-function for usage in other cases than frontmattered-markdown
+const origSaveTiddlerToFile = $tw.utils.saveTiddlerToFile;
+
+/*jslint node: true, browser: true */
+/*global $tw: false */
+"use strict";
+
+var fs = require("fs"),
+	path = require("path");
+
+/*
+Save a tiddler to a file described by the fileInfo:
+	filepath: the absolute path to the file containing the tiddler
+	type: the type of the tiddler file (NOT the type of the tiddler)
+	hasMetaFile: true if the file also has a companion .meta file
+*/
+exports.saveTiddlerToFile = function(tiddler,fileInfo,callback) {
+
+	if(fileInfo.type === "text/x-frontmattered-markdown" && tiddler.fields["#yaml-frontmatter-fields"]) {
+		$tw.utils.createDirectory(path.dirname(fileInfo.filepath));
+
+		var typeInfo = $tw.config.contentTypeInfo[tiddler.fields.type || "text/plain"] || {encoding: "utf8"};
+		const fields4Frontmatter = $tw.utils.parseStringArray(tiddler.fields["#yaml-frontmatter-fields"], false);
+		const fieldValues = [];
+		for(const frontmatterField of fields4Frontmatter){
+			if(frontmatterField.endsWith("::list")){
+				const tiddlerField = frontmatterField.substring(0, frontmatterField.length - "::list".length);
+				const parsedFields = $tw.utils.parseStringArray(tiddler.fields[tiddlerField], false) || [];
+				let propertyString = tiddlerField + ": \n";
+				for (var i = 0; i < parsedFields.length; i++) {
+					propertyString += "- " + parsedFields[i];
+					if(i < parsedFields.length-1) {
+						propertyString += "\n";
+					}
+				}
+				if(parsedFields.length === 0) propertyString += "-"
+				fieldValues.push(propertyString);
+			}else{
+				fieldValues.push(frontmatterField + ": " + (tiddler.fields[frontmatterField] ? tiddler.fields[frontmatterField] : ""));
+			}
+		}
+
+		const markdownPayload = "---\n" + fieldValues.join("\n\n") + "\n---\n" + tiddler.fields.text;
+		fs.writeFile(fileInfo.filepath, markdownPayload, typeInfo.encoding, function (err) {
+			if (err) {
+				return callback(err);
+			}
+			return callback(null, fileInfo);
+		});
+	}else{
+		origSaveTiddlerToFile(tiddler,fileInfo,callback);
+	}
+}
+
+})();
---
+++ startup.js
@@ -0,0 +1,110 @@
+/*\
+title: $:/plugins/yaml-frontmattered-markdown/startup.js
+type: application/javascript
+module-type: startup
+
+Title, stylesheet and page rendering
+
+\*/
+(function(){
+
+/*jslint node: true, browser: true */
+/*global $tw: false */
+"use strict";
+
+// Export name and synchronous status
+exports.name = "frontmatterer";
+exports.platforms = ["node"];
+exports.after = ["startup"];
+exports.synchronous = true;
+
+/*
+// Default story and history lists
+var PAGE_TITLE_TITLE = "$:/core/wiki/title";
+var PAGE_STYLESHEET_TITLE = "$:/core/ui/PageStylesheet";
+var PAGE_TEMPLATE_TITLE = "$:/core/ui/RootTemplate";
+
+// Time (in ms) that we defer refreshing changes to draft tiddlers
+var DRAFT_TIDDLER_TIMEOUT_TITLE = "$:/config/Drafts/TypingTimeout";
+var THROTTLE_REFRESH_TIMEOUT = 400;
+
+ */
+exports.startup = function() {
+	$tw.utils.each($tw.wiki.filterTiddlers("[type[text/x-markdown]]"),function(item) {
+		const markdownTiddler = $tw.wiki.getTiddler(item);
+		const fields = $tw.utils.deepCopy(markdownTiddler.fields);
+		// SEE https://regex101.com/r/60KP75/2 (DELETE: https://regex101.com/delete/mz5TYrHsDVmCPg1ZUcTJIDSv)
+		const match = markdownTiddler.fields.text.match(/^(-{3}(?:\r\n|\r|\n)(.*?)(?:\r\n|\r|\n)-{3}(?:\r\n|\r|\n)?)?(.*)/ms);
+		if(match && match[2]) {
+			fields.text = match[3];
+			//$tw.wiki.addTiddler(new $tw.Tiddler(markdownTiddler));
+			const newTiddler = new $tw.Tiddler(deserializeYAMLFrontmatterComment(match, fields));
+			$tw.wiki.addTiddler(newTiddler);
+		}
+	});
+};
+
+	var deserializeYAMLFrontmatterComment = function(match,fields) {
+		//read only property-names and store-them (keep ordering, when writing back)
+		// See https://regex101.com/r/YmeCHK/1 (DELETE: https://regex101.com/delete/dFGAoWkb7q8z7Ri0RcY4lM9N)
+		const propertyNames = match[2].match(/^\w.*?(?=:)/gm);
+
+		//handle multiline-props
+		// SEE https://regex101.com/r/yByhLr/3 (DELETE: https://regex101.com/delete/HbIqLUi4lig7GO5Hn5fbqnPw)
+		const multilinePropMatches = match[2].match(/^\w.*?$(?:(?:\r\n|\r|\n)(?=(?:\W*)?-)^(?:\W*)?-.*?$)+/gm)
+		if(multilinePropMatches){
+			for (const multilineProp of multilinePropMatches) {
+				const lines = multilineProp.split(/(?:\r\n|\r|\n)/);
+				const propName = lines[0].substring(0, multilineProp.indexOf(":")).trim();
+				for (let j=0; j<propertyNames.length; j++) {
+					if (propertyNames[j].match(propName)) {
+						propertyNames[j] += "::list";
+						break;
+					}
+				}
+				const values = [];
+				for (var i = 1; i < lines.length; i++) {
+					values.push(lines[i].substring(lines[i].indexOf("-")+1).trim());
+				}
+				const propValue = $tw.utils.stringifyList(values).trim();
+				fields[propName] = propValue;
+			}
+		}
+		//handle single-props
+		// SEE https://regex101.com/r/Hcp77a/1 (DELETE: https://regex101.com/delete/AEnuzg1It0e2qI5c2dml6bqb)
+		const singlelinePropMatches = match[2].match(/(?:^\w.*?$)(?!(?:\W*)?-)/gm)
+		if(singlelinePropMatches){
+			for (const singlelineProp of singlelinePropMatches) {
+				const propName = singlelineProp.substring(0, singlelineProp.indexOf(":")).trim();
+				const propValue = singlelineProp.substring(singlelineProp.indexOf(":")+1).trim();
+				if(propValue.startsWith("[") && propValue.endsWith("]")){
+					//single-line list (e.g. [a, "b", c])
+					const values = propValue.substring(1, propValue.length - 1).split(",");
+					const valuesList = [];
+					for (let value of values) {
+						value = value.trim();
+						if(value.startsWith("\"") || value.startsWith("'")){
+							value = value.substring(1, value.length - 1).trim();
+						}
+						valuesList.push(value);
+					}
+					for (let j=0; j<propertyNames.length; j++) {
+						if (propertyNames[j].match(propName)) {
+							propertyNames[j] += "::list";
+							break;
+						}
+					}
+					const propValues = $tw.utils.stringifyList(valuesList);
+					fields[propName] = propValues;
+				}else {
+					fields[propName] = propValue;
+				}
+			}
+		}
+		fields["#yaml-frontmatter-fields"] = propertyNames.join(" ");
+		fields.type = "text/x-frontmattered-markdown";
+
+		return fields;
+	};
+
+})();
